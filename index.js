d15a866d799fa99f16edbcef7ea57b561024ced7
aniyazov-varta <arkadiy.niyazov@gmail.com>
2026-04-25 15:37:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const OPEN_HOUR = 9;
-const CLOSE_HOUR = 17;
+const OPEN_HOUR   = 8;
+const OPEN_MINUTE = 30;
+const CLOSE_HOUR  = 17;
+const CLOSE_MINUTE = 0;
 const TIMEZONE = 'America/New_York';
-const OPEN_DAYS = [0,1,2,3,4,5,6];
+const OPEN_DAYS = [1,2,3,4,5];
 
 const GREETING_BIN_URL   = 'https://api.telnyx.com/v2/media/Greeting_for_1800_number.xml';
 const AFTERHOURS_BIN_URL = 'https://api.telnyx.com/v2/media/AfterHours.xml';
@@ -16,8 +18,12 @@ function isBusinessHours() {
   const now = new Date().toLocaleString('en-US', { timeZone: TIMEZONE });
   const d = new Date(now);
   const hour = d.getHours();
+  const minute = d.getMinutes();
   const day = d.getDay();
-  return OPEN_DAYS.includes(day) && hour >= OPEN_HOUR && hour < CLOSE_HOUR;
+  const timeNow  = hour * 60 + minute;
+  const timeOpen = OPEN_HOUR * 60 + OPEN_MINUTE;
+  const timeClose = CLOSE_HOUR * 60 + CLOSE_MINUTE;
+  return OPEN_DAYS.includes(day) && timeNow >= timeOpen && timeNow < timeClose;
 }
 
 app.post('/voice', (req, res) => {
