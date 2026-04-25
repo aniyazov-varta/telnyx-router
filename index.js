7d543a840c55dc39093ff8de1a6065c00b033649
aniyazov-varta <arkadiy.niyazov@gmail.com>
2026-04-25 12:33:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // --- YOUR SETTINGS ---
-const OPEN_HOUR = 9;       // 9am
-const CLOSE_HOUR = 17;     // 5pm
+const OPEN_HOUR = 9;
+const CLOSE_HOUR = 17;
 const TIMEZONE = 'America/New_York';
-const OPEN_DAYS = [1,2,3,4,5]; // Mon=1, Fri=5 (0=Sun, 6=Sat)
+const OPEN_DAYS = [1,2,3,4,5];
 
 const GREETING_BIN_URL   = 'https://api.telnyx.com/v2/media/Greeting_for_1800_number.xml';
 const AFTERHOURS_BIN_URL = 'https://api.telnyx.com/v2/media/AfterHours.xml';
@@ -20,12 +23,16 @@ function isBusinessHours() {
 }
 
 app.post('/voice', (req, res) => {
-  const url = isBusinessHours() ? GREETING_BIN_URL : AFTERHOURS_BIN_URL;
+  const open = isBusinessHours();
+  console.log(`Call received. Business hours: ${open}`);
+  const url = open ? GREETING_BIN_URL : AFTERHOURS_BIN_URL;
   res.set('Content-Type', 'text/xml');
   res.send(`<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Redirect>${url}</Redirect>
 </Response>`);
 });
+
+app.get('/', (req, res) => res.send('Telnyx router is running.'));
 
 app.listen(3000, () => console.log('Running on port 3000'));
